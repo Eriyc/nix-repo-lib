7e93c5267aab7cf5e7469bb34d83b53989a31a7a
eric <eric@dgren.dev>
2026-03-04 07:57:14 +0100

    chore(release): v1.0.1

--- template/flake.nix
+++ template/flake.nix
@@ -4,7 +4,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    devshell-lib.url = "git+https://git.dgren.dev/eric/nix-flake-lib?ref=v1.0.0";
+    devshell-lib.url = "git+https://git.dgren.dev/eric/nix-flake-lib?ref=v1.0.1";
     devshell-lib.inputs.nixpkgs.follows = "nixpkgs";
   };
 
